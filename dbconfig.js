e3f02339f6c414e493aed24d40825d058289c310
IbrahimSaffi <ibrahimsaffinew@gmail.com>
2022-09-05 11:45:31 +0500

    commit

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,29 @@
-const pool = require("pg").Pool;
+// const pool = require("pg").Pool;
+// // postgres://xkzyigaq:EzTjRZDyMIBw1QPX6VVYShGWER1_HNpD@jelani.db.elephantsql.com/xkzyigaq
+// const dbPool = new pool({
+//     user:"xkzyigaq",
+//     host:"jelani.db.elephantsql.com",
+//     database:"xkzyigaq",
+//     password:"EzTjRZDyMIBw1QPX6VVYShGWER1_HNpD",
+//     port:5432
+// });
+// module.exports = dbPool;
+var pg = require('pg');
+//or native libpq bindings
+//var pg = require('pg').native
 
-const dbPool = new pool({
-    user:"postgres",
-    host:"127.0.0.1",
-    database:"shorturl",
-    password:"jmk161651",
-    port:5432
+var conString = "postgres://xkzyigaq:EzTjRZDyMIBw1QPX6VVYShGWER1_HNpD@jelani.db.elephantsql.com/xkzyigaq" //Can be found in the Details page
+var client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+  });
 });
-module.exports = dbPool;
+module.exports = client
